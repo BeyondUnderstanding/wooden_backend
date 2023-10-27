0b1c79869ae3094b472eb245f3c6d198f45dda44
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-10-27 19:51:52 +0300

    models refactor

---
+++ modules/admin/orders/schema.py
@@ -0,0 +1,25 @@
+import datetime
+from typing import List
+
+from pydantic import BaseModel
+
+from modules.admin.games.schema import GameSchemaWithID
+
+
+class AGameToOrderSchema(BaseModel):
+    game: GameSchemaWithID
+
+
+class AOrderFullSchema(BaseModel):
+    id: int
+    date: datetime.datetime
+    client_name: str
+    client_phone: str
+    is_payed: bool
+    is_refunded: bool
+    is_canceled: bool
+    legal_id: str
+    has_manager: bool
+    managers_count: int
+    total_price: float
+    games: List[AGameToOrderSchema]
