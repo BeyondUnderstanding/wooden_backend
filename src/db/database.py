58536b994d95a29b5bf4a20849e66c87d3aeabbe
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-11-14 19:58:28 +0300

    idk

--- src/db/database.py
+++ src/db/database.py
@@ -6,14 +6,17 @@ from src.config import DB_BASE, DB_USER, DB_SERVER, DB_PASSWORD
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}/{DB_BASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                        pool_size=10,
-                        max_overflow=2,
+                        pool_size=20,
+                        max_overflow=20,
                         pool_recycle=300,
                         pool_pre_ping=True,
                         pool_use_lifo=True,
                         echo_pool=True
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            # expire_on_commit=True,
+                            bind=engine)
 
 
 # https://github.com/tiangolo/full-stack-fastapi-postgresql/blob/master/%7B%7Bcookiecutter.project_slug%7D%7D/backend/app/app/db/base_class.py
