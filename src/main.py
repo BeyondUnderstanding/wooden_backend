33c732dd0878d8b464aef11583f57742b157125b
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-11-15 20:02:19 +0300

    Sentry Init

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+import sentry_sdk
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 
 from src.db.database import engine
 from src.modules.router import main_router
+
+sentry_sdk.init(
+    dsn="https://b538fbcfd6bd4cf12de8abe05edba57b@o4506230928834560.ingest.sentry.io/4506230929883138",
+    enable_tracing=True,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 app = FastAPI(title='Wooden Games API')
 app.add_middleware(
