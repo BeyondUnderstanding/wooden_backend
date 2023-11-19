67ab3815d1563e8a9a46ecd9bf015560b953ea61
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-11-19 17:52:21 +0300

    Secure admin methods

--- src/modules/admin/orders/grid/router.py
+++ src/modules/admin/orders/grid/router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
+from fastapi_jwt import JwtAuthorizationCredentials
 from sqlalchemy.orm import Session
 
+from src.modules.admin.auth.router import access_security
 from src.db.database import get_db
 from src.modules.admin.orders.grid.schema import CloseTimeslotModel
 from src.modules.exceptions import NOT_IMPLEMENTED
@@ -9,15 +11,18 @@ grid_router = APIRouter(prefix='/grid')
 
 
 @grid_router.post('/close', tags=['Grid'])
-async def close_timeslot(data: CloseTimeslotModel, session: Session = Depends(get_db)):
+async def close_timeslot(data: CloseTimeslotModel, session: Session = Depends(get_db),
+                         auth: JwtAuthorizationCredentials = Security(access_security)):
     raise NOT_IMPLEMENTED
 
 
 @grid_router.post('/open', tags=['Grid'])
-async def open_timeslot(session: Session = Depends(get_db)):
+async def open_timeslot(session: Session = Depends(get_db),
+                        auth: JwtAuthorizationCredentials = Security(access_security)):
     raise NOT_IMPLEMENTED
 
 
 @grid_router.post('/transfer', tags=['Grid'])
-async def transfer_order_to_another_timeslot(session: Session = Depends(get_db)):
+async def transfer_order_to_another_timeslot(session: Session = Depends(get_db),
+                                             auth: JwtAuthorizationCredentials = Security(access_security)):
     raise NOT_IMPLEMENTED
--- src/modules/admin/orders/router.py
+++ src/modules/admin/orders/router.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
+from fastapi_jwt import JwtAuthorizationCredentials
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
@@ -10,6 +11,7 @@ from src.models import Book, GameToBook, Game
 from .grid.router import grid_router
 from src.modules.exceptions import NOT_IMPLEMENTED
 from .schema import OrderModel, OrderModelFull
+from ..auth.router import access_security
 
 orders_router = APIRouter(prefix='/orders')
 orders_router.include_router(grid_router)
@@ -17,7 +19,7 @@ orders_router.include_router(grid_router)
 
 @orders_router.get('/', tags=['Orders'], response_model=List[OrderModel])
 async def get_all(start: int = 0, size: int = 10, payed_only: bool = False,
-                  session: Session = Depends(get_db)):
+                  session: Session = Depends(get_db), auth: JwtAuthorizationCredentials = Security(access_security)):
     return session.scalars(select(Book)
                            .distinct(Book.id)
                            .where(Book.is_payed.in_([True] if payed_only else (True, False)))
@@ -27,13 +29,13 @@ async def get_all(start: int = 0, size: int = 10, payed_only: bool = False,
 
 @orders_router.get('/{order_id}', tags=['Orders'], response_model=OrderModelFull)
 async def get_one(order_id: int,
-                  session: Session = Depends(get_db)):
+                  session: Session = Depends(get_db), auth: JwtAuthorizationCredentials = Security(access_security)):
     return session.get(Book, order_id)
 
 
 @orders_router.delete('/{order_id}/cancel', tags=['Orders'])
 async def cancel_order(order_id: int,
-                       session: Session = Depends(get_db)):
+                       session: Session = Depends(get_db), auth: JwtAuthorizationCredentials = Security(access_security)):
     order = session.get(Book, order_id)
     order.is_canceled = True
     if order.is_payed:
@@ -51,6 +53,6 @@ async def cancel_order(order_id: int,
 
 
 @orders_router.post('/{order_id}/sms', tags=['Orders'])
-async def send_message(order_id: int):
+async def send_message(order_id: int, auth: JwtAuthorizationCredentials = Security(access_security)):
     # TODO: Send customer custom message
     raise NOT_IMPLEMENTED
