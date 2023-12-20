7be4766960f9b85432a4daa43c1cf091ae965362
Evgeniy Finskiy <finskiy.ru@yandex.ru>
2023-12-21 02:20:43 +0300

    CryptoCom integration

--- src/modules/integrations/cryptocom/router.py
+++ src/modules/integrations/cryptocom/router.py
@@ -1,15 +1,17 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Security, Header
+from fastapi import APIRouter, Depends, Security, Header, Request
 from fastapi_jwt import JwtAuthorizationCredentials
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
+from src.config import CRYPTO_SECRET
 from src.db.database import get_db
 from src.modules.admin.auth.router import access_security
 from src.modules.client.basket.schema import CreateOrderOK
 from src.modules.integrations.cryptocom.schema import CryptoWebhook
 from src.modules.integrations.cryptocom.utils import crypto_create_payment
+import hashlib, hmac
 
 cryptocom = APIRouter(prefix='/payments/crypto', tags=['Payments'])
 
@@ -25,10 +27,27 @@ def test_crypto(order_id: int, amount: float,
         checkout_url=obj.payment_url
     )
 
+
 @cryptocom.post('/webhook')
-def crypto_webhook(data: CryptoWebhook, Pay_Signature: Annotated[str, Header(convert_underscores=True)]):
-    order_id = int(data.data.object.order_id)
-    status = data.data.object.status == 'succeeded'
-    print(order_id, status)
-    print(Pay_Signature)
+async def crypto_webhook(data: CryptoWebhook,
+                         Pay_Signature: Annotated[str, Header(convert_underscores=True)],
+                         request: Request):
+    t, v1 = Pay_Signature.split(',')
+    t = t.split('=')
+    v1 = v1.split('=')
+    request_body = (await request.body()).decode('utf-8')
+    validating_string = ((f'{t[1]}.' + request_body
+                         .replace('None', 'null')
+                         .replace("'", '"')
+                         .replace('False', 'false')
+                         .replace('True', 'true')
+                         .replace(' ', ''))
+                         .replace('\n', ''))
+    print(validating_string)
+    secret = 'VgVU6FUXOGCeRVvw25dz69b93HGS2d9FiSBC40zuXO0='.encode('utf-8')
+    sign = hmac.new(secret, validating_string.encode('utf-8'), hashlib.sha256)
+    print(sign.hexdigest())
+    print(v1[1])
+    # print(order_id, status)
+    # print(Pay_Signature)
     return JSONResponse(content={'status': 'ok'}, status_code=200)
